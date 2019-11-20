67816d7e854c8fe2f337e3016feb96435a863eec
Giacomo Magnanini <giacomo@kinfe.it>
2019-11-20 17:03:33 +0100

    fixed umbrella apps config

---
+++ apps/engine/config/config.exs
@@ -0,0 +1,3 @@
+import Config
+
+config :opus, :instrumentation, Engine.Steps.Logging
--- config/config.exs
+++ config/config.exs
@@ -16,4 +16,6 @@ import Config
 #       format: "$date $time [$level] $metadata$message\n",
 #       metadata: [:user_id]
 #
-config :opus, :instrumentation, Engine.Steps.Logging
+for config <- "../apps/*/config/config.exs" |> Path.expand(__DIR__) |> Path.wildcard() do
+  import_config config
+end
