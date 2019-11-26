48a10bacbef73de246c51609f84e34ee029b6ab1
Giacomo Magnanini <giacomo@kinfe.it>
2019-11-26 22:46:52 +0100

    added dialyxir

--- mix.exs
+++ mix.exs
@@ -6,16 +6,20 @@ defmodule Poker.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: releases()
     ]
   end
 
-  # Dependencies listed here are available only for this
-  # project and cannot be accessed from applications inside
-  # the apps folder.
-  #
-  # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [{:dialyxir, "~> 0.5", only: [:dev], runtime: false}]
+  end
+
+  defp releases do
+    [
+      demo: [
+        applications: [engine: :permanent, frontend: :permanent]
+      ]
+    ]
   end
 end
